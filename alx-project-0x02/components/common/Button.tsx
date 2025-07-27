58499f9797f134e068e1db033e02bb5e09b17a6e
Sebrin <sebrinmudesir@gmail.com>
2025-07-27 21:24:26 +0300

    Finished week five task five.

--- alx-project-0x02/components/common/Button.tsx
+++ alx-project-0x02/components/common/Button.tsx
@@ -1,11 +1,11 @@
-import { ButtonProps } from "@/interfaces";  // Import the interface we just created
+import { ButtonProps } from "@/interfaces";  
 
 export const Button = ({ size, shape, children }: ButtonProps) => {
-  // Size classes mapping
+ 
   const sizeClasses = {
-    small: 'py-1 px-2 text-sm',  // Small button styles
-    medium: 'py-2 px-4 text-base',  // Medium button styles
-    large: 'py-3 px-6 text-lg'  // Large button styles
+    small: 'py-1 px-2 text-sm',  
+    medium: 'py-2 px-4 text-base', 
+    large: 'py-3 px-6 text-lg' 
   };
 
   return (
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,5 +1,6 @@
-import Header from "@/components/layout/Header"
 import { Button} from "@/components/common/Button";
+import Header from "@/components/layout/Header"
+
 export default function AboutPage() {
   return (
     <div>
