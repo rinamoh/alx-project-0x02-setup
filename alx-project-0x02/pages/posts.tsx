c7f4b4344325c8b1d090df845303aef5a92b2720
Sebrin <sebrinmudesir@gmail.com>
2025-07-27 22:08:23 +0300

    Finished task seven of week five.

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,24 +2,18 @@ import Header from "@/components/layout/Header";
 import { PostProps } from "@/interfaces";
 import { PostCard } from "@/components/common/PostCard";
 import { useState,useEffect } from "react";
-export default function Posts() {
-
-    const [posts, setPosts] = useState<PostProps[]>([]); 
-    useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())  
-      .then(data => setPosts(data));     
-  }, []); 
+export default function Posts({ posts }: { posts: PostProps[] }) {
   return (
-  <div>
+    <div>
       {posts.map(post => (
-        <PostCard
-          key={post.id}      // Unique key for React list rendering
-          title={post.title} 
-          body={post.body}    
-          userId={post.userId} 
-        />
+        <PostCard key={post.id} {...post} />
       ))}
     </div>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await res.json()
+  return { props: { posts } }
 }
